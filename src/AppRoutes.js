dd39959b9fdb88c37c17b8b0f32d6cab0dc91191
Suresh Dukiya <rjsh1503@gmail.com>
2022-09-17 21:32:34 +0530

    first commit

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import {Index } from './Pages/Index';
+import {Home} from "./Pages/Home"
+import {NotFound} from './Pages/NotFound';
+
+
 
 export const AppRoutes = () => {
     return (
-
+            <Routes>
+                <Route path='/' element={<Index></Index>}></Route>
+                <Route path='/home' element={<Home></Home>}></Route>
+                <Route path='/*' element={<NotFound></NotFound>}></Route>
+            </Routes>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,15 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
+import {Index} from "../Pages/Index"
+import { Home } from "../Pages/Home"
 
 export const NavBar = () =>{
     return(
-
+        <nav>
+            <ul>
+                <li><Link class="index-link" to="/" element={<Index></Index>}>Index</Link></li>
+                <li><Link class="home-link" to="/home" element={<Home></Home>}>Home</Link></li>
+            </ul>
+        </nav>
     )
 }
